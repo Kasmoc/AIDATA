52fd819925938b2d2b792319ecfeba959fe1c4db
Kasmoc <Smihay24@student.aau.dk>
2025-04-09 12:43:27 +0200

    b

--- clean.py
+++ clean.py
@@ -9,23 +9,9 @@ file_path = "C:\\Users\\tonny\\OneDrive\\Desktop\\DAKI2 - Repo\\AI & Data\\Minip
 
 raw_df = pd.read_csv(file_path)
 
-# Select numerical columns only
-numerical_columns = raw_df.select_dtypes(include=['int64', 'float64']).columns
+#Imputation for remaining collumns
+data = pd.get_dummies(df, columns=['Gender', 'Blood Type', 'Diabetes', 'Smoking'])
 
-# Create correlation matrix
-correlation_matrix = raw_df[numerical_columns].corr()
+imputer = SimpleImputer(strategy='median')
+data[['BMI', 'Temperature']] = imputer.fit_transform(data[['BMI', 'Temperature']])
 
-# Create a figure with a larger size
-plt.figure(figsize=(12, 10))
-
-# Create heatmap
-sns.heatmap(correlation_matrix, 
-            annot=True,  # Show correlation values
-            cmap='coolwarm',  # Color scheme
-            center=0,  # Center the colormap at 0
-            fmt='.2f',  # Round to 2 decimal places
-            square=True)  # Make the plot square
-
-# Customize the plot
-plt.title('Correlation Matrix of Medical Students Dataset Features')
-plt.tight_layout()  # Adjust layout to prevent label cutoff
